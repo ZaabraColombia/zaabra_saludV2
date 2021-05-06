5560dafdb5419c0dfb216119bfcb660402542c73
jhonzaabra <72825636+jhonzaabra@users.noreply.github.com>
2021-05-06 09:43:43 -0500

    jhonf 06-05-2021 09:43

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,90 @@
+<!-- Footer -->
+<footer>
+    <!-- Footer Desktop -->
+    <div class="container-fluid p-0">
+        <!-- row newzletter-->
+        <div class="row footer_newsletter">
+            <!-- column titulo-->
+            <div class="col-10 newsletter_contenido">
+                <h2 class="newsletter_titulo">¡Suscríbete a nuestro newsletter!</h2>
+                <p class="newsletter_texto"> Mantente al día de todas nuestras novedades </p>
+            </div>
+            <!-- column end titulo -->
+
+            <!-- column form suscribirme-->
+            <div class="col-5 suscribirme_contenido">
+                <form  class="row form_suscribirme" action="" id="" method="get">
+                    <input class="col-md-8 input_suscribirme" type="email" id="txtNewLeeter" name="txtNewLeeter" placeholder="zaabra@gmail.com">
+                    <button class="col-md-3 boton_suscribirme" type="submit" value="Suscribirme">
+                    <span> Suscribirme </span>
+                    <img src="" class="boton_icon-flecha" alt=""> 
+                </form>
+            </div>
+            <!-- column end suscribirme -->
+
+            <!-- column terminos -->
+            <div class="col-md-8 terminos_contenido">
+                <input class="checkBox_terminos" type="checkbox" id="aceptoTerminosNewLetter">
+                <h4 class="texto_terminos">Acepto <b><u>términos y condiciones</u></b> y autorizo el <b><u>tratamiento de mis datos personales</u></b></h4>
+            </div>
+            <!-- column end terminos -->
+        </div>
+        <!-- row end newsletter -->
+
+        <!--Grid row-->
+        <div class="row">
+            <!--Grid column-->
+            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                <h5 class="text-uppercase">Links</h5>
+
+                <ul class="list-unstyled mb-0">
+                <li>
+                    <a href="#!" class="text-dark">Link 1</a>
+                </li>
+                <li>
+                    <a href="#!" class="text-dark">Link 2</a>
+                </li>
+                <li>
+                    <a href="#!" class="text-dark">Link 3</a>
+                </li>
+                <li>
+                    <a href="#!" class="text-dark">Link 4</a>
+                </li>
+                </ul>
+            </div>
+            <!--Grid column-->
+
+            <!--Grid column-->
+            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                <h5 class="text-uppercase mb-0">Links</h5>
+
+                <ul class="list-unstyled">
+                <li>
+                    <a href="#!" class="text-dark">Link 1</a>
+                </li>
+                <li>
+                    <a href="#!" class="text-dark">Link 2</a>
+                </li>
+                <li>
+                    <a href="#!" class="text-dark">Link 3</a>
+                </li>
+                <li>
+                    <a href="#!" class="text-dark">Link 4</a>
+                </li>
+                </ul>
+            </div>
+            <!--Grid column-->
+        </div>
+        <!--Grid row-->
+    </div>
+    <!-- Footer Desktop -->
+
+    <!-- Copyright -->
+    <div class="col-12 footer_copy-right">
+      <span class="texto_copy-right">&copy; Zaabra 2021 </span>  
+    </div>
+    <!-- Copyright -->
+</footer>
+<!-- Footer -->
+
+
