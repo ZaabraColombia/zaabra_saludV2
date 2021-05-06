34ea1f8089adc5270a07ae4bce8670fdcca3ef59
importadoraAndiamos <70965330+importadoraAndiamos@users.noreply.github.com>
2021-05-06 10:51:40 -0500

    oscar 06-05-01

---
+++ resources/views/seccionesHome/principal.blade.php
@@ -0,0 +1,15 @@
+<!-- Contenedor banner imagen -->
+
+
+
+<div class="container-fluid container-imagen-index">
+    <picture>
+        <source srcset="{{$urlZaabra}}img/banners/principalSP/bannerprincipalmobile.jpg" media="(max-width: 600px)">
+        <img srcset="{{ $urlZaabra.$objbannersPrincipalSP[0]->$ruta}}" class="container-imagen-index">
+    </picture>
+</div>
+
+
+
+
+
