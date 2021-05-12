8b97fd6da09412809719b3fdc23044d78910c0a3
jhonzaabra <72825636+jhonzaabra@users.noreply.github.com>
2021-05-12 17:25:13 -0500

    jhonf 12-05-2021 17:25

--- app/Http/Controllers/profesionesController.php
+++ app/Http/Controllers/profesionesController.php
@@ -36,21 +36,21 @@ class profesionesController extends Controller
         return $consultaBannerProfesiones;
     }
 
-        // consulta para cargar todas las profesiones disponibles y que esten activas
-        public function cargarProfesiones(){
-            return DB::select('SELECT pr.nombreProfesion, pr.descripcion, pr.urlimagen, pr.idProfesion
-            FROM profesiones  pr
-            where pr.estado <>0 ORDER BY orden ASC');
-        }
+    // consulta para cargar todas las profesiones disponibles, activas y en el orden secuencial según diseño
+    public function cargarProfesiones(){
+        return DB::select('SELECT pr.nombreProfesion, pr.descripcion, pr.urlimagen, pr.idProfesion
+        FROM profesiones  pr
+        where pr.estado <>0 ORDER BY orden ASC');
+    }
 
-        // consulta para cargar carrusel profesiones 
-        public function cargarCarruselProfesiones(){
-            $consultaCarruselProfesiones = DB::table('ventabanners')
-            ->select('rutaImagenVenta')
-            ->where('aprobado', '<>', 0)
-            ->where('idtipobanner', '=', 6)
-            ->get();
-            return $consultaCarruselProfesiones;
-        }
+    // consulta para cargar carrusel profesiones 
+    public function cargarCarruselProfesiones(){
+        $consultaCarruselProfesiones = DB::table('ventabanners')
+        ->select('rutaImagenVenta')
+        ->where('aprobado', '<>', 0)
+        ->where('idtipobanner', '=', 6)
+        ->get();
+        return $consultaCarruselProfesiones;
+    }
 
 }
