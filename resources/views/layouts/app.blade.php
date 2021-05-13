a0b4b37ade5afedca832a60290a795656ec859d9
Nicolas-Moreno-arevalo <75846359+Nicolas-Moreno-arevalo@users.noreply.github.com>
2021-05-13 15:18:21 -0500

    melopela-13-05-21

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,8 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="{{ asset('js/sliders.js') }}" defer></script>
+    <script src="{{ asset('js/home.js') }}" defer></script>
+    <script src="{{ asset('js/galeriaProfesiones.js') }}" defer></script>
     <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
 
     <!-- Fonts -->
