8f036ebd757ce47c1b652ff91745739085b4d08a
importadoraAndiamos <70965330+importadoraAndiamos@users.noreply.github.com>
2021-05-13 14:55:37 -0500

    oscar 13-05-2021

--- app/Http/Controllers/medicosEspecialidadController.php
+++ app/Http/Controllers/medicosEspecialidadController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class medicosEspecialidadController extends Controller
 {
@@ -10,12 +11,11 @@ class medicosEspecialidadController extends Controller
 
     public function index($idEspecialidad)
     {
-        $objcarruselprofesionalespremiun= $this->cargarCarruselProfesionalesPremiun();
+        $objcarruselprofesionalespremiun= $this->cargarCarruselProfesionalesPremiun($idEspecialidad);
         $objmedicospagonormal  = $this->cargarMedicosPagoNormal($idEspecialidad);
         $objmedicossinpago  = $this->cargarMedicosSinPago($idEspecialidad);
         $objcarruselPublicidadprofesionales = $this->cargarCarruselProfesionales();
 
-        dd($objcarruselprofesionalespremiun);
         return view('Profesionales', compact(
             'objcarruselprofesionalespremiun',
             'objmedicospagonormal',
@@ -25,12 +25,56 @@ class medicosEspecialidadController extends Controller
 
     }
 
-         // consulta para cargar todas los profesionales segun su profesion 
-         public function cargarCarruselProfesionalesPremiun($idProfesion){
-            return DB::select('SELECT es.urlimagen, es.nombreEspecialidad, es.idEspecialidad
-            FROM profesiones pr
-            INNER JOIN  especialidades es ON pr.idProfesion = es.idProfesion
-            WHERE  es.estado <>0  AND es.idProfesion=?', [$idProfesion]);
+         // consulta para cargar todas los profesionales segun su especialidad y que pagan premiun
+         public function cargarCarruselProfesionalesPremiun($idEspecialidad){
+            return DB::select("SELECT us.primernombre, us.primerapellido, ep.nombreEspecialidad, mn.nombre, pf.descripcionPerfil, un.nombreuniversidad, pf.fotoperfil
+            FROM  users us
+            INNER JOIN pagos pg ON us.id=pg.idUsuario
+            INNER JOIN perfilesprofesionales pf ON us.id=pf.idUser
+            INNER JOIN municipio mn ON pf.id_municipio=mn.id_municipio
+            INNER JOIN especialidades ep ON pf.idespecialidad=ep.idEspecialidad
+            INNER JOIN perfilesprofesionalesuniversidades pu ON pf.idPerfilProfesional=pu.idPerfilProfesional
+            INNER JOIN universidades un ON pu.id_universidad=un.id_universidad
+            WHERE pg.idtipopago=12 AND pf.idespecialidad=$idEspecialidad AND pf.aprobado<>0");
+        }
+
+            // consulta para cargar todas los profesionales segun su especialidad y el pago normal
+            public function cargarMedicosPagoNormal($idEspecialidad){
+            return DB::select("SELECT us.primernombre, us.primerapellido, ep.nombreEspecialidad, mn.nombre, pf.descripcionPerfil, un.nombreuniversidad, pf.fotoperfil
+            FROM  users us
+            INNER JOIN pagos pg ON us.id=pg.idUsuario
+            INNER JOIN perfilesprofesionales pf ON us.id=pf.idUser
+            INNER JOIN municipio mn ON pf.id_municipio=mn.id_municipio
+            INNER JOIN especialidades ep ON pf.idespecialidad=ep.idEspecialidad
+            INNER JOIN perfilesprofesionalesuniversidades pu ON pf.idPerfilProfesional=pu.idPerfilProfesional
+            INNER JOIN universidades un ON pu.id_universidad=un.id_universidad
+            WHERE pg.idtipopago=13 AND pf.idespecialidad=$idEspecialidad AND pf.aprobado<>0");
+        }
+
+            // consulta para cargar todas los profesionales segun su especialidad y el pago normal
+            public function cargarMedicosSinPago($idEspecialidad){
+            return DB::select("SELECT us.primernombre, us.primerapellido, ep.nombreEspecialidad
+            FROM  users us
+            INNER JOIN pagos pg ON us.id=pg.idUsuario
+            INNER JOIN perfilesprofesionales pf ON us.id=pf.idUser
+            INNER JOIN municipio mn ON pf.id_municipio=mn.id_municipio
+            INNER JOIN especialidades ep ON pf.idespecialidad=ep.idEspecialidad
+            INNER JOIN perfilesprofesionalesuniversidades pu ON pf.idPerfilProfesional=pu.idPerfilProfesional
+            INNER JOIN universidades un ON pu.id_universidad=un.id_universidad
+            WHERE pg.idtipopago=14 AND pf.idespecialidad=$idEspecialidad AND pf.aprobado<>0");
+        }
+
+
+            // consulta para cargar carrusel profesionales 
+            public function cargarCarruselProfesionales(){
+            $consultaCarruselProfesiones = DB::table('ventabanners')
+            ->select('rutaImagenVenta')
+            ->where('aprobado', '<>', 0)
+            ->where('idtipobanner', '=', 10)
+            ->get();
+            return $consultaCarruselProfesiones;
         }
 
 }
+
+
