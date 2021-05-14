8fce653723261af0a16275d58c9933e456a1a542
importadoraAndiamos <70965330+importadoraAndiamos@users.noreply.github.com>
2021-05-14 10:40:42 -0500

    oscar 14-05-2021

--- routes/web.php
+++ routes/web.php
@@ -23,11 +23,11 @@ Auth::routes(['verify' => true]);
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('verified');
 
 /*Esta ruta es de galeria profesiones y dirige al controlador encargado de traer la informacion a la vista*/
-Route:: get('galeriaProfesiones',[App\Http\Controllers\profesionesController::class,'index'])->name('galeriaProfesiones');
+Route:: get('/Profesiones',[App\Http\Controllers\profesionales\profesionesController::class,'index'])->name('Profesiones');
 
 /*Esta ruta es de galeria especialidades y dirige al controlador encargado de traer la informacion a la vista*/
-Route:: get('galeriaespecialidades/{idProfesion}',[App\Http\Controllers\especialidadesController::class,'index'])->name('galeriaespecialidades');
+Route:: get('/Especialidades/{idProfesion}',[App\Http\Controllers\profesionales\especialidadesController::class,'index'])->name('Especialidades');
 
 /*Esta ruta es de galeria profesionales y dirige al controlador encargado de traer la informacion a la vista*/
-Route:: get('Profesionales/{idEspecialidad}',[App\Http\Controllers\medicosEspecialidadController::class,'index'])->name('Profesionales');
+Route:: get('/Profesionales/{idEspecialidad}',[App\Http\Controllers\profesionales\medicosEspecialidadController::class,'index'])->name('Profesionales');
 
