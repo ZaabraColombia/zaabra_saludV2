324c4408ba61efd5d191e2df47cf11e0d3647a3c
jhonzaabra <72825636+jhonzaabra@users.noreply.github.com>
2021-05-21 17:33:01 -0500

    jhonf 21-05-2021 17:32

--- public/js/header.js
+++ public/js/header.js
@@ -8,3 +8,12 @@ function ocultaInput(){
    let myinput = document.querySelector.bind(document);
    myinput(".contains_barra").classList.toggle("barra_busqueda-mobile")
 }
+
+!function() {
+   let selector = document.querySelector.bind(document);
+   let queryRuta = window.location.pathname;
+
+   if (queryRuta.includes("register") || queryRuta.includes("login") ) {
+      selector(".footer_newsletter").style.display = "none";
+   }
+}();
