0dde7c3cbce3efe3ce4b1bb7c16efd31a12455d9
jhonzaabra <72825636+jhonzaabra@users.noreply.github.com>
2021-05-25 17:34:19 -0500

    jhonf 25-05-2021 17:34

--- public/js/register.js
+++ public/js/register.js
@@ -1,3 +1,4 @@
+// Funcion para ocultar y mostrar elementos en la vista de Register
 function hideForm (z){
     let myvar = z.getAttribute('data-position');
     
@@ -8,12 +9,15 @@ function hideForm (z){
         selector(".names_institution").style.display = "none";
     }
 
+    else if (myvar == "doctor") {
+        selector(".names_person").style.display = "block";
+        selector(".datos_secundarios").style.display = "block";
+        selector(".names_institution").style.display = "none";
+    }
+
     else if (myvar == "institucion") {
         selector(".names_institution").style.display = "block";
         selector(".datos_secundarios").style.display = "block";
         selector(".names_person").style.display = "none";
     }
-}
-function cambiarImagenJS(){
-    document.getElementById("#idrol1").style.src="/img/iconos/icono-paciente-amarillo.svg";
 }
