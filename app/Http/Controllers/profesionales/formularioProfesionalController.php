2bf5a24a2f26628a7bf202905626dda90d037536
importadoraAndiamos <70965330+importadoraAndiamos@users.noreply.github.com>
2021-05-31 19:19:46 -0500

    oscar 31-05-2021

--- app/Http/Controllers/profesionales/formularioProfesionalController.php
+++ app/Http/Controllers/profesionales/formularioProfesionalController.php
@@ -17,6 +17,7 @@ use App\Models\universidades;
 use App\Models\perfilesprofesionalesuniversidades;
 use File;
 
+
 class formularioProfesionalController extends Controller
 {
 
@@ -33,98 +34,207 @@ class formularioProfesionalController extends Controller
         $universidades = universidades::all();
         $id_user=auth()->user()->id;/*id usuario logueado*/
         $objuser = $this->cargaDatosUser($id_user);
+        $objFormulario1=$this->cargaFormulario1($id_user);
+
+
 
         return view('profesionales.FormularioProfesional',compact(
         'objuser',
         'area',
         'pais',
-        'universidades'
+        'universidades',
+        'objFormulario1'
         ));
     }
 
 
 
-/*------------------------------------- inicio json busqueda area. profesion y especialidad----------------------*/
+    /*------------------------------------- inicio json busqueda area. profesion y especialidad----------------------*/
 
- public function getprofesion(Request $request){
-     $profesion = profesiones::where("idArea",$request->idArea)->get();
-     return response()->json($profesion);
- }
- public function getespecialidad(Request $request){
-     $especialidad = especialidades::where("idProfesion",$request->idProfesion)->get();
-     return response()->json($especialidad);
- }
-/*------------------------------------- fin json busqueda area. profesion y especialidad----------------------*/
-
-
-
-/*------------------------------------- inicio json busqueda departamento, provincia, ciudad----------------------*/
-    public function getDepartamento(Request $request){
-
-       $departamento = departamento::where("id_pais",$request->id_pais)->get();
-        return response()->json($departamento);
+    public function getprofesion(Request $request){
+        $profesion = profesiones::where("idArea",$request->idArea)->get();
+        return response()->json($profesion);
     }
-    public function getProvincia(Request $request){
-        $provincia = provincia::where("id_departamento",$request->id_departamento)->get();
-        return response()->json($provincia);
+    public function getespecialidad(Request $request){
+        $especialidad = especialidades::where("idProfesion",$request->idProfesion)->get();
+        return response()->json($especialidad);
     }
-    public function getCiudad(Request $request){
-        $municipio = municipio::where("id_provincia",$request->id_provincia)->get();
-        return response()->json($municipio);
+    /*------------------------------------- fin json busqueda area. profesion y especialidad----------------------*/
+
+
+
+    /*------------------------------------- inicio json busqueda departamento, provincia, ciudad----------------------*/
+        public function getDepartamento(Request $request){
+
+        $departamento = departamento::where("id_pais",$request->id_pais)->get();
+            return response()->json($departamento);
+        }
+        public function getProvincia(Request $request){
+            $provincia = provincia::where("id_departamento",$request->id_departamento)->get();
+            return response()->json($provincia);
+        }
+        public function getCiudad(Request $request){
+            $municipio = municipio::where("id_provincia",$request->id_provincia)->get();
+            return response()->json($municipio);
+        }
+    /*------------------------------------- fin json busqueda departamento, provincia, ciudad----------------------*/
+
+
+
+
+    /*------------inicio busquedad datos basicos usuario logueado y data resgistrada del proesional-----------------*/
+
+        public function cargaDatosUser($id_user){
+        return DB::select("SELECT us.primernombre, us.segundonombre, us.primerapellido, us.segundoapellido
+        FROM users us
+        WHERE id=$id_user");
     }
-/*------------------------------------- fin json busqueda departamento, provincia, ciudad----------------------*/
-
-
-
-
-/*-------------------------------------inicio busquedad datos basicos usuario logueado----------------------*/
-
-    public function cargaDatosUser($id_user){
-    return DB::select("SELECT us.primernombre, us.segundonombre, us.primerapellido, us.segundoapellido
-    FROM users us
-    WHERE id=$id_user");
-}
-/*-------------------------------------fin busquedad datos basicos usuario logueado----------------------*/
-
-
-
-
-
-/**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-
-    protected function create(Request $request)
-    {
-
-      
-        /*id usuario logueado*/
-        $id_user=auth()->user()->id;
-        /*captura el nombre del logo*/
-        $nombrelogo=$request->logo->getClientOriginalName();
-
-        /*crea una nueva carpeta con el id del usuario*/
-        $path = public_path().'img/user/' . $id_user;
-        File::makeDirectory($path,  0777, true);
-
-        /*guarda la imagen en carpeta con el id del usuario*/
-        $image = $request->file('logo');
-        $image->move("img/user/$id_user", $image->getClientOriginalName());
-      
-        /*guarda la informacion en la base de datos*/
-        $infoProf = new perfilesprofesionales;
-        $infoProf->idUser = $id_user;
-        $infoProf->idarea = $request->idArea ;
-        $infoProf->idprofesion = $request->profesion ;
-        $infoProf->idespecialidad = $request->especialidad ;
-        $infoProf->fechanacimiento = $request->fecha ;
-        $infoProf->numeroTarjeta = $request->tarjeta ;
-        $infoProf->imglogoempresa = "img/user/$id_user/$nombrelogo";
-        $infoProf->save();
+ 
+    
+    public function cargaFormulario1($id_user){
+    return DB::select("SELECT pf.direccion,  pf.genero, pf.EmpresaActual, 
+    pf.imglogoempresa, pf.fechanacimiento, pf.numeroTarjeta, pf.entidadCertificoTarjeta,
+    pf.descripcionPerfil,ar.idarea, ar.nombreArea ,pr.idprofesion, pr.nombreProfesion, 
+    ep.idEspecialidad,  ep.nombreEspecialidad, p.id_pais,p.nombre, de.id_departamento, de.nombre,
+    prv.id_provincia,prv.nombre, mu.id_municipio, mu.nombre
+    FROM perfilesprofesionales pf
+    INNER JOIN users us   ON pf.idUser=us.id
+    LEFT JOIN  areas ar ON pf.idarea= ar.idArea
+    LEFT JOIN  profesiones pr ON pf.idprofesion= pr.idProfesion
+    LEFT JOIN  especialidades ep ON pf.idespecialidad= ep.idEspecialidad
+    LEFT JOIN  pais p ON pf.idpais=p.id_pais
+    LEFT JOIN  departamentos de ON pf.id_departamento= de.id_departamento
+    LEFT JOIN  provincias prv ON pf.id_provincia= prv.id_provincia
+    LEFT JOIN  municipios mu ON pf.id_municipio= mu.id_municipio
+    WHERE pf.idUser=$id_user");
     }
+    /*------------ Fin inicio busquedad datos basicos usuario logueado y data resgistrada del proesional-----------------*/
+
+
+
+
+        /**
+         * Get a validator for an incoming registration request.
+         *
+         * @param  array  $data
+         * @return \Illuminate\Contracts\Validation\Validator
+         */
+
+
+    /*-------------------------------------Creacion y/o modificacion formulario 1----------------------*/
+        protected function create1(Request $request)
+        {
+            $index = $this->index();
+           
+            /*id usuario logueado*/
+            $id_user=auth()->user()->id;
+
+           /*consulta si existe el profesional*/
+           $idexisteperfil = DB::table('perfilesprofesionales')
+           ->select('idPerfilProfesional')
+           ->join('users', 'perfilesprofesionales.idUser', '=', 'users.id')
+           ->where('perfilesprofesionales.idUser', $id_user)
+           ->first();
+
+           /*valido que el profesional no exista para que cree uno nuevo en caso contrario lo modifique */
+           if(is_null($idexisteperfil)){
+                    /*captura el nombre del logo*/
+                    $nombrelogo=$request->logo->getClientOriginalName();
+
+                    /*crea una nueva carpeta con el id del perfil nuevo*/
+                    $path = public_path().'img/user/' . $id_user;
+                    File::makeDirectory($path,  0777, true);
+
+                    /*guarda la imagen en carpeta con el id del usuario*/
+                    $image = $request->file('logo');
+                    $image->move("img/user/$id_user", $image->getClientOriginalName());
+                
+                    /*anexo iduser y img logoempresa  al request*/
+                    $request->merge([
+                        'idUser' => "$id_user", 
+                        'imglogoempresa' => "img/user/$id_user/$nombrelogo"
+                    ]);
+                
+                    perfilesprofesionales::create($request->all());
+
+                   /*consulta el perfil registrado para despues guardarlo en la tabla de perfilesprofesionalesuniversidades*/
+                    $idPerfilProfesional = DB::table('perfilesprofesionales')
+                    ->select('idPerfilProfesional')
+                    ->join('users', 'perfilesprofesionales.idUser', '=', 'users.id')
+                    ->where('perfilesprofesionales.idUser', $id_user)
+                    ->first();
+
+                    foreach($idPerfilProfesional as $idPerfilProfesional){
+                        $idProProfesi=$idPerfilProfesional;
+                    }
+
+                    /*anexo iduser y img logoempresa  al request*/
+                    $request->merge([
+                        'idPerfilProfesional' => "$idProProfesi"
+                        ]);  
+                    perfilesprofesionalesuniversidades::create($request->all());
+
+                return($index);
+
+            }else{
+
+                    /*captura el nombre del logo*/
+                    $nombrelogo=$request->logo->getClientOriginalName();
+
+
+                    /*guarda la imagen en carpeta con el id del usuario*/
+                    $image = $request->file('logo');
+                    $image->move("img/user/$id_user", $image->getClientOriginalName());
+                
+                    /*anexo iduser y img logoempresa  al request*/
+                    $request->merge([
+                        'idUser' => "$id_user", 
+                        'imglogoempresa' => "img/user/$id_user/$nombrelogo"
+                    ]);
+
+                    $dataPerfilesprofesionales = request()->all();
+                    unset($dataPerfilesprofesionales['_token']);
+                    unset($dataPerfilesprofesionales['id_universidad']);
+                    unset($dataPerfilesprofesionales['logo']);
+                   
+           
+                    perfilesprofesionales::where('idUser', $id_user)->update($dataPerfilesprofesionales);
+
+                    /*consulta el perfil registrado para despues guardarlo en la tabla de perfilesprofesionalesuniversidades*/
+                    $idPerfilProfesional = DB::table('perfilesprofesionales')
+                    ->select('idPerfilProfesional')
+                    ->join('users', 'perfilesprofesionales.idUser', '=', 'users.id')
+                    ->where('perfilesprofesionales.idUser', $id_user)
+                    ->first();
+
+                    foreach($idPerfilProfesional as $idPerfilProfesional){
+                        $idProProfesi=$idPerfilProfesional;
+                    }
+
+                    /*anexo el id del profesional*/
+                    $request->merge([
+                        'idPerfilProfesional' => "$idProProfesi"
+                        ]);  
+
+                    $perfilesprofesionalesuniversidades = request()->all();
+                    unset($perfilesprofesionalesuniversidades['_token']);
+                    unset($perfilesprofesionalesuniversidades['fechanacimiento']);
+                    unset($perfilesprofesionalesuniversidades['numeroTarjeta']);
+                    unset($perfilesprofesionalesuniversidades['imglogoempresa']);
+                    unset($perfilesprofesionalesuniversidades['idarea']);
+                    unset($perfilesprofesionalesuniversidades['idprofesion']);
+                    unset($perfilesprofesionalesuniversidades['idespecialidad']);
+                    unset($perfilesprofesionalesuniversidades['idUser']);
+                    unset($perfilesprofesionalesuniversidades['logo']);
+
+                    perfilesprofesionalesuniversidades::where('idPerfilProfesional', $idProProfesi)->update($perfilesprofesionalesuniversidades);
+                    
+                    return($index);
+                 
+            }
+
+        }
+/*-------------------------------------Fin Creacion y/o modificacion formulario 1----------------------*/
 
 
 }
--- resources/views/profesionales/FormularioProfesional.blade.php
+++ resources/views/profesionales/FormularioProfesional.blade.php
@@ -3,86 +3,165 @@
 @section('content')
 
 <div class="container">
-
-<div class="row">
+<!------------------------------primera parte del formulario---------------------------------------> 
+<div class="row" style="background: red;">
   <div class="col-md-10 col-md-offset-1">
     <div class="panel panel-default">
       <div class="panel-heading">Agregar archivos</div>
         <div class="panel-body">
           <form method="POST" action="{{ url ('/FormularioProfesionalSave') }}" method="POST" enctype="multipart/form-data" accept-charset="UTF-8">
-            
-            <input type="hidden" name="_token" value="{{ csrf_token() }}">
-            
-            <div class="col-12">
-            @foreach ($objuser as $objuser)
-                <input value="{{$objuser->primernombre}}" readonly></input>
-                <input value="{{$objuser->segundonombre}}" readonly></input>
-                <input value="{{$objuser->primerapellido}}" readonly></input>
-                <input value="{{$objuser->segundoapellido}}" readonly></input>
-            @endforeach
-            </div>
-            <div class="form-group col-12">
-                <label for="example-date-input" class="col-2 col-form-label">Date</label>
-                <div class="col-10">
-                    <input class="form-control" type="date" value="2011-08-19" id="example-date-input" name="fecha">
-                </div>
-            </div>
-            <div class="col-12">
-                <div class="form-group">
-                    <select id="area" name="idArea" class="form-control" style="width:350px" >
-                        <option value="" selected disabled>Seleccione area</option>
-                            @foreach($area as $area)
-                        <option value="{{$area->idArea}}"> {{$area->nombreArea}}</option>
-                            @endforeach
-                    </select>
-                </div>
+          <input type="hidden" name="_token" value="{{ csrf_token() }}"> 
+           <!---------------valida que ya exista informacion y la muestra
+            en caso contrario muestra un formulario vacio---------------------> 
 
-                <div class="form-group">
-                    <label for="title">Selecione Profesion:</label>
-                      <select name="profesion" id="profesion" class="form-control" style="width:350px">
-                    </select>
-                </div>
-                <div class="form-group">
-                    <label for="title">Seleccione especialidad:</label>
-                       <select name="especialidad" id="especialidad" class="form-control" style="width:350px">
-                    </select>
-                </div>
-            </div>
-            <div class="col-12">
-                <div class="form-group">
-                    <label for="title">Selecione Universidad:</label>
-                    <select  class="form-control" style="width:350px" name="universidad">
-                        <option value="">Seleccione Universidad</option>
-                            @foreach($universidades as $universidades)
-                        <option value="{{$universidades->id_universidad}}"> {{$universidades->nombreuniversidad}}</option>
-                            @endforeach
-                    </select>
-                </div>
-            </div>
-            <div class="col-12">
-                <div class="form-group">
-                    <label for="title">Tarjeta Profesional</label>
-                    <input id="tarjeta" placeholder="nombre" type="number" name="tarjeta">
-                </div>
-            </div>
-            <div class="form-group">
-              <label class="col-md-4 control-label">Nuevo Archivo</label>
-              <div class="col-md-6">
-                <input type="file" class="form-control" name="logo"  id="seleccionArchivos" accept="image/png, image/jpeg">
-                <img id="imagenPrevisualizacion">
-              </div>
-            </div>
+          @if(!empty($objFormulario1))
+                       @foreach ($objFormulario1 as $objFormulario1)
+                            <input value="{{$objFormulario1->imglogoempresa}}" readonly></input>
+                        @endforeach
+          
+                    <div class="col-12">
+                        @foreach ($objuser as $objuser)
+                            <input value="{{$objuser->primernombre}}" readonly></input>
+                            <input value="{{$objuser->segundonombre}}" readonly></input>
+                            <input value="{{$objuser->primerapellido}}" readonly></input>
+                            <input value="{{$objuser->segundoapellido}}" readonly></input>
+                        @endforeach
+                    </div>
+                    @foreach ($objFormulario1 as $objFormulario1)
+                        <div class="form-group col-12">
+                            <label for="example-date-input" class="col-2 col-form-label">Date</label>
+                            <div class="col-10">
+                                <input class="form-control" type="date" value="{{$objFormulario1->fechanacimiento}}" id="example-date-input" name="fechanacimiento">
+                            </div>
+                        </div>
+                    @endforeach
+                    <div class="col-12">
+                        <div class="form-group">
+                            <select id="idarea" name="idarea" class="form-control" style="width:350px" >
 
-            <div class="form-group">
-              <div class="col-md-6 col-md-offset-4">
-                <button type="submit" class="btn btn-primary">Enviar</button>
-              </div>
-            </div>
+                                <option value="" selected disabled>Seleccione area</option>
+
+                                    @foreach($area as $area)
+                                <option value="{{$area->idArea}}"> {{$area->nombreArea}}</option>
+                                    @endforeach
+                            </select>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="title">Selecione Profesion:</label>
+                            <select name="idprofesion" id="idprofesion" class="form-control" style="width:350px">
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="title">Seleccione especialidad:</label>
+                            <select name="idespecialidad" id="idespecialidad" class="form-control" style="width:350px">
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="form-group">
+                            <label for="title">Selecione Universidad:</label>
+                            <select  class="form-control" style="width:350px" name="id_universidad">
+                                <option value="">Seleccione Universidad</option>
+                                    @foreach($universidades as $universidades)
+                                <option value="{{$universidades->id_universidad}}"> {{$universidades->nombreuniversidad}}</option>
+                                    @endforeach
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="form-group">
+                            <label for="title">Tarjeta Profesional</label>
+                            <input id="tarjeta" placeholder="nombre" type="number" name="numeroTarjeta">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                    <label class="col-md-4 control-label">Nuevo Archivo</label>
+                    <div class="col-md-6">
+                        <input type="file" class="form-control" name="logo"  id="seleccionArchivos" accept="image/png, image/jpeg">
+                        <img id="imagenPrevisualizacion">
+                    </div>
+                    </div>
+
+                    <div class="form-group">
+                    <div class="col-md-6 col-md-offset-4">
+                        <button type="submit" class="btn btn-primary">Enviar</button>
+                    </div>
+                    </div>
+                <!--------------- Fin campos llenos --------------------->     
+            @else
+                    <div class="col-12">
+                        @foreach ($objuser as $objuser)
+                            <input value="{{$objuser->primernombre}}" readonly></input>
+                            <input value="{{$objuser->segundonombre}}" readonly></input>
+                            <input value="{{$objuser->primerapellido}}" readonly></input>
+                            <input value="{{$objuser->segundoapellido}}" readonly></input>
+                        @endforeach
+                    </div>
+                    <div class="form-group col-12">
+                        <label for="example-date-input" class="col-2 col-form-label">Date</label>
+                        <div class="col-10">
+                            <input class="form-control" type="date" value="2011-08-19" id="example-date-input" name="fechanacimiento">
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="form-group">
+                            <select id="idarea" name="idarea" class="form-control" style="width:350px" >
+                                <option value="" selected disabled>Seleccione area</option>
+                                    @foreach($area as $area)
+                                <option value="{{$area->idArea}}"> {{$area->nombreArea}}</option>
+                                    @endforeach
+                            </select>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="title">Selecione Profesion:</label>
+                            <select name="idprofesion" id="idprofesion" class="form-control" style="width:350px">
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="title">Seleccione especialidad:</label>
+                            <select name="idespecialidad" id="idespecialidad" class="form-control" style="width:350px">
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="form-group">
+                            <label for="title">Selecione Universidad:</label>
+                            <select  class="form-control" style="width:350px" name="id_universidad">
+                                <option value="">Seleccione Universidad</option>
+                                    @foreach($universidades as $universidades)
+                                <option value="{{$universidades->id_universidad}}"> {{$universidades->nombreuniversidad}}</option>
+                                    @endforeach
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="form-group">
+                            <label for="title">Tarjeta Profesional</label>
+                            <input id="tarjeta" placeholder="nombre" type="number" name="numeroTarjeta">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                    <label class="col-md-4 control-label">Nuevo Archivo</label>
+                    <div class="col-md-6">
+                        <input type="file" class="form-control" name="fotoperfil"  id="seleccionArchivos" accept="image/png, image/jpeg">
+                        <img id="imagenPrevisualizacion">
+                    </div>
+                    </div>
+
+                    <div class="form-group">
+                    <div class="col-md-6 col-md-offset-4">
+                        <button type="submit" class="btn btn-primary">Enviar</button>
+                    </div>
+                </div>
+            @endif
           </form>
         </div>
       </div>
     </div>
   </div>
+  <!------------------------------Fin primera parte del formulario---------------------------------------> 
 </div>
 
 
