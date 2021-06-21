6e4fef990b6792d7841af9431a80112041f666bf
importadoraAndiamos <70965330+importadoraAndiamos@users.noreply.github.com>
2021-06-21 10:43:52 -0500

    oscar 21-06-2021

---
+++ app/Http/ViewComposers/HeaderComposer.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\ViewComposers;
+use Illuminate\View\View;
+use Illuminate\Support\Facades\DB;
+
+class HeaderComposer{
+
+
+    public function compose(view $view){
+        $user=Auth::id()
+
+        $tipoUsuario = DB::table('users')
+        ->select(DB::raw('users.id,users_roles.idrol'))
+        ->join('users_roles', 'users.id', '=', 'users_roles.iduser')
+        ->where('users.id', '=',$user)
+        ->first();
+
+        $view->with('objtipoUsuarioLogueado'=>$tipoUsuario);
+    }
+
+}
