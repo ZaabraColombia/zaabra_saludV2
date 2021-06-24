70c62b6a474395296eb3c6a379f1be0120507f8a
Nicolas-Moreno-arevalo <75846359+Nicolas-Moreno-arevalo@users.noreply.github.com>
2021-06-24 17:39:59 -0500

    nicolas-24-06-21

--- public/js/perfil-instituciones.js
+++ public/js/perfil-instituciones.js
@@ -36,4 +36,45 @@ document.addEventListener('DOMContentLoaded', function () {
         }
       });
 
+      const swiper_certificados = new Swiper(".swiper_certificados", {
+
+        loop: true,
+       
+        autoplay: {
+          delay: 3500,
+          disableOnInteraction: false,
+        },
+      
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+      
+        breakpoints: {
+           // when window width is >= 320px
+          320: {
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+          },
+           // when window width is >= 1024px
+          1024: {
+            slidesPerView: 3,
+            slidesPerGroup: 1,
+            spaceBetween: 20,
+          },
+            // when window width is >= 1600px
+          1600: {
+            slidesPerView: 3,
+            slidesPerGroup: 1,
+            spaceBetween: 20,
+          },
+        }
+      });
+
+});
+
+ // Función para cambiar de color y dejar un solo item desplegado en las opciones de las tarjetas de la vista "membresiaProfesional" y "membresiaInstitucion" 
+ $('.desplegable_institucion .containt_options-collapse-membresia').on( "click", function() {
+  $(this).siblings().find(".boton_collapse-off-membresia").removeClass("boton_collapse-on-institucion");
+  $(this).find(".boton_collapse-off-membresia").toggleClass("boton_collapse-on-institucion");
 });
