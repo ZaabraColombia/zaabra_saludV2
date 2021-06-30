d73979e75b16ab2b4fc12b25b6740916a6ed8d46
Nicolas-Moreno-arevalo <75846359+Nicolas-Moreno-arevalo@users.noreply.github.com>
2021-06-30 09:01:54 -0500

    nicolas-30-06-21

--- public/js/perfil-instituciones.js
+++ public/js/perfil-instituciones.js
@@ -72,28 +72,38 @@ document.addEventListener('DOMContentLoaded', function () {
       });
       
       const menu_insti = [].slice.apply(document.querySelectorAll('.item_landing_insti'))
-      const sections_insti = [].slice.apply(document.querySelectorAll('.sections_insti'))
+      const sections_insti = [].slice.apply(document.querySelectorAll('.section_insti'))
 
       document.querySelector('.landingInsti').addEventListener('click', el => {
+
+        let count;
 
         if (el.target.classList.contains('item_landing_insti')) {
 
            let itera_insti = menu_insti.indexOf(el.target)
-           sections_insti.map(seccionInst => seccionInst.classList.remove('sections_insti_active'))
+        
+           sections_insti.map(seccionInst => seccionInst.classList.remove('section_insti_active'));
            menu_insti.map(menuInst => menuInst.classList.remove(
              'servicios_clicked',
              'acerca_clicked',
              'certificados_clicked',
              'sedes_clicked',
-             'gale-inst_clicked'))
+             'gale-inst_clicked'));
 
+             sections_insti[itera_insti].classList.toggle('section_insti_active');
+            
+            (itera_insti == 0) ? menu_insti[0].classList.toggle('servicios_clicked'): count++
+            (itera_insti == 1) ? menu_insti[1].classList.toggle('acerca_clicked'): count++
+            (itera_insti == 2) ? menu_insti[2].classList.toggle('certificados_clicked'): count++
+            (itera_insti == 3) ? menu_insti[3].classList.toggle('sedes_clicked'): count++
+            (itera_insti == 4) ? menu_insti[4].classList.toggle('gale-inst_clicked'): count++
         }
       })
 
 });
 
  // Función para cambiar de color y dejar un solo item desplegado en las opciones de las tarjetas de la vista "membresiaProfesional" y "membresiaInstitucion" 
- $('.desplegable_institucion .containt_options-collapse-membresia').on( "click", function() {
+ $('.boton_collapse-off-institucion').on( "click", function() {
   $(this).siblings().find(".boton_collapse-off-membresia").removeClass("boton_collapse-on-institucion");
   $(this).find(".boton_collapse-off-membresia").toggleClass("boton_collapse-on-institucion");
 });
