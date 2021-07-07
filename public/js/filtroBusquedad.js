79ba0ec247425c86d74d58d5d450024c4638bb39
jhonzaabra <72825636+jhonzaabra@users.noreply.github.com>
2021-07-07 11:09:17 -0500

    jhonf 07-07-2021

--- public/js/filtroBusquedad.js
+++ public/js/filtroBusquedad.js
@@ -1,4 +1,4 @@
-ruta='http://localhost:8000/';
+ruta='http://127.0.0.1:8000/';
 $(function(){
     $("#filtro").autocomplete({
         autoFocus: true,
@@ -37,49 +37,3 @@ $(function(){
              
     }
 });
-
-
-
-$(function(){
-    $("#filtro2").autocomplete({
-        autoFocus: true,
-    source: function(request, response) {
-        $.ajax({
-        url: ruta+"search/filtro",
-        dataType: "json",
-        data: {
-            term: request.term
-        },
-        success: function( data ) {
-            response( data );
-        }
-        });  
-    },
-    
-    select: function (event, ui) {
-        $('#barra_busqueda2').val(ui.item.id); // save selected id to input
-        // Set selection
-        $('#barra_busqueda2').keypress(function(e){
-            var keycode = (e.keyCode ? e.keyCode : e.which);
-            if (keycode == '13') {
-                e.preventDefault();
-                window.location = ui.item.id;
-              }
-        });
-
-     }
-    })
-    .data( "ui-autocomplete" )._renderItem = function( ul, item ) { 
-        return $( "<li></li>" )  
-            .data( "item.autocomplete", item )  
-            .append( "<a href='"  + item.id + "'>" +item.label +"</a>" )   
-            .append( "<div class='dropdown-divider m-0'></div>" ) 
-            .appendTo( ul ); 
-             
-    }
-});
-
-
-
-
-
