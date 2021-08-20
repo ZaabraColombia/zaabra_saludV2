dfe812922c6f2614777d1a9df4d96311d756706f
jhonzaabra <72825636+jhonzaabra@users.noreply.github.com>
2021-08-20 16:26:46 -0500

    jhonf 20-08-2021 16:26

--- app/Http/Controllers/admin/adminHistoriaClinicaProfesional.php
+++ app/Http/Controllers/admin/adminHistoriaClinicaProfesional.php
@@ -10,4 +10,24 @@ class adminHistoriaClinicaProfesional extends Controller
     public function index(){
         return view('panelAdministrativoProf.historiaClinicaProfesional');
     }
+
+    public function registrar(){
+        return view('panelAdministrativoProf.registroPaciente');
+    }
+
+    public function registro(){
+        return view('panelAdministrativoProf.pacienteRegistrado');
+    }
+
+    public function consulta(){
+        return view('panelAdministrativoProf.editarConsulta');
+    }
+
+    public function patologia(){
+        return view('panelAdministrativoProf.editarPatologia');
+    }
+
+    public function Expediente(){
+        return view('panelAdministrativoProf.editarExpediente');
+    }
 }
