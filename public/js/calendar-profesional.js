fd3e3ec41e273e8f6474a26af4cbbe823c93e23e
Nicolas-Moreno-arevalo <75846359+Nicolas-Moreno-arevalo@users.noreply.github.com>
2021-08-25 16:23:36 -0500

    avance de calendario profecional

--- public/js/calendar-profesional.js
+++ public/js/calendar-profesional.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar-profesional');
 
+    var count = 0;
 
     var calendar = new FullCalendar.Calendar(calendarEl, {
         selectable: true,
@@ -12,6 +13,7 @@ document.addEventListener('DOMContentLoaded', function() {
         locale: 'es',
         events: [
             {
+                id: count,
                 title: 'paciente 1',
                 start: '2021-08-22',
                 especialidad: 'especialidad',
@@ -22,7 +24,7 @@ document.addEventListener('DOMContentLoaded', function() {
         dateClick: function(info) {
             console.log(info.dateStr);
             //limpiar el formulario
-            //$('#form-agendar-cita-profesional')[0].reset();
+            $("#form-agendar-cita-profesional")[0].reset();
             $('#fecha_input-profesional').val(info.dateStr);
             $('#agregar-cita-profesional').modal('show');
         },
@@ -32,12 +34,17 @@ document.addEventListener('DOMContentLoaded', function() {
         eventClick: function(info) {
             console.log(info.event);
 
+            //Llenar el modal con la informacion
             $('#nombre_paciente-profesional').html(info.event.title);
             $('#especialidad-profesional').html(info.event.extendedProps.especialidad);
             $('#fecha-profesional').html(moment(info.event.start).format('dddd, D MMMM'));
             $('#hora-profesional').html(moment(info.event.start).format('hh:mm A '));
             $('#tipo_cita-profesional').html(info.event.extendedProps.tipo_cita);
 
+            // ID del evento
+            $('#editar-cita-btn-profesional').data('id', info.event.id);
+
+            //Activar modal
             $('#ver-cita-profecional').modal();
         }
     });
@@ -50,12 +57,73 @@ document.addEventListener('DOMContentLoaded', function() {
         var fecha = moment($('#fecha_input-profesional').val() + " " + $('#hora_input-profesional').val() , "YYYY-MM-DD H:mm").format();
         console.log(fecha);
         calendar.addEvent({
+            id: count,
             title: $('#paciente_input-profesional').val(),
             start: fecha,
             especialidad: $('#especialidad_input-profesional').val(),
             //paciente: 'paciente 1',
-            tipo_cita: $('#tipo_cita_select-profesional').val()
+            tipo_cita: $('#tipo_cita_select-profesional').val(),
+            //color: 'blue',
+            //textColor: 'white'
         });
         $('#agregar-cita-profesional').modal('hide');
+        count++;
     });
+
+    $('#editar-cita-btn-profesional').click(function (e) {
+        var id = $(this).data('id');
+
+        var event = calendar.getEventById(id);
+
+        console.log(event);
+
+        //Llenar el modal con la informacion
+        $('#paciente_input-editar-profesional').val(event.title);
+        $('#fecha_input-editar-profesional').val(moment(event.start).format('YYYY-MM-DD'));
+        $('#hora_input-editar-profesional').val(moment(event.start).format('HH:mm'));
+        $('#tipo_cita-profesional>option[value=' + event.extendedProps.tipo_cita +  ']').attr('selected', 'selected');
+
+        $('#ver-cita-profecional').modal('hide');
+        $('#editar-cita-model-profesional').modal('show');
+
+    });
+
+    $('#editar-cita-profesional').click(function (e){
+        var fecha = moment($('#fecha_input-editar-profesional').val() + " " + $('#hora_input-profesional').val() , "YYYY-MM-DD H:mm").format();
+
+        var id = $(this).data('id');
+
+        var event = calendar.getEventById(id);
+
+        // event.title = $('#paciente_input-editar-profesional').val();
+        // event.start = fecha;
+        // event.extendedProps.especialidad =  $('#especialidad_input-profesional').val();
+        // event.extendedProps.tipo_cita = fecha;
+
+        /*calendar.event({
+            id: id,
+            title: $('#paciente_input-editar-profesional').val(),
+            start: fecha,
+            especialidad: $('#especialidad_input-profesional').val(),
+            //paciente: 'paciente 1',
+            tipo_cita: $('#tipo_cita_select-editar-profesional').val()
+        });*/
+
+
+        $('#editar-cita-model-profesional').modal('hide');
+    });
+
+    $('#cancelar-cita-btn-profesional').click(function (e) {
+        var id = $(this).data('id');
+
+        var event = calendar.getEventById(id);
+
+        //event.remove();
+
+        $('#ver-cita-profecional').modal('hide');
+        $('#cancelada-cita-modal-profecional').modal('show');
+    });
+
+
+
 });
--- resources/views/panelAdministrativoProf/calendarioProfesional.blade.php
+++ resources/views/panelAdministrativoProf/calendarioProfesional.blade.php
@@ -16,7 +16,7 @@
                     <h2 class="dias"><i></i> Días disponibles</h2>
                 </div>
             </div>
-            <div class="col-md-7 col-sm-12 p-0">
+            <div class="col-12 col-lg-9 p-0">
                 <div id="calendar-profesional"></div>
             </div>
         </div>
@@ -49,9 +49,11 @@
 
                 <!-- Sección botón Pagar -->
                 <div class="modal-footer section_btn_citas">
-                    <button type="submit" class="btnAgendar-popup" id="" data-toggle="modal" data-target="#exampleModal2" onclick="elementClose(this)" data-position="edito">Editar cita</button>
 
-                    <button type="submit" class="btnCancelar-popup" id="" data-toggle="modal" data-target="#exampleModal3" onclick="elementClose(this)" data-position="cancelar">Cancelar cita </button>
+                    <button type="button" class="btnAgendar-popup" id="editar-cita-btn-profesional" data-id>Editar cita</button>
+
+                    <button type="button" class="btnCancelar-popup" id="cancelar-cita-btn-profesional">Cancelar cita </button>
+
                 </div>
             </div>
         </div>
@@ -71,7 +73,7 @@
                 </div>
 
                 <div class="modal-body modal_headerCitas">
-                    <form method="POST" action="" id="form-agendar-cita-profecional">
+                    <form method="POST" action="" id="form-agendar-cita-profesional">
                         <div class="col-md-6 p-0">
                             <label for="especialidad_input-profesional" class="col-12 text_label-formProf">Especialidad</label>
                             <input class="form-control" type="text" value="{{ auth()->user()->profecional->profecion->nombreProfesion }}" id="especialidad_input-profesional" name="especialidad_input-profesional" readonly>
@@ -111,8 +113,62 @@
         </div>
     </div>
 
+    <!-- Pop-up editar cita -->
+    <div class="modal fade modalD" id="editar-cita-model-profesional" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal_citas_popUp" role="document">
+            <div class="modal-content content_modalCitas">
+                <!-- Sección boton derecho de cierre "X" -->
+                <div class="modal-header modal_headerCitas">
+                    <h1 class="title_popup_miCita" id="exampleModalLabel">Agendar cita</h1>
+
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+                <div class="modal-body modal_headerCitas">
+                    <form method="POST" action="" id="form-agendar-cita-profesional">
+                        <div class="col-md-6 p-0">
+                            <label for="especialidad_input-profesional" class="col-12 text_label-formProf">Especialidad</label>
+                            <input class="form-control" type="text" value="{{ auth()->user()->profecional->profecion->nombreProfesion }}" id="especialidad_input-profesional" name="especialidad_input-profesional" readonly>
+                        </div>
+
+                        <div class="col-md-6 p-0">
+                            <label for="tipo_cita_select-editar-profesional" class="col-12 text_label-formProf">Tipo de cita</label>
+                            <select id="tipo_cita_select-editar-profesional" class="form-control" name="tipo_cita_select-editar-profesional">
+                                <option value="Presencial"> Presencial </option>
+                                <option value="Virtual"> Virtual </option>
+                            </select>
+                        </div>
+
+                        <div class="col-md-6 p-0">
+                            <label for="paciente_input-editar-profesional" class="col-12 text_label-formProf">Nombre Paciente</label>
+                            <input class="form-control" type="text" id="paciente_input-editar-profesional" name="paciente_input-editar-profesional">
+                        </div>
+
+                        <div class="col-md-6 p-0">
+                            <label for="fecha_input-editar-profesional" class="col-12 text_label-formProf">Fecha</label>
+                            <input class="form-control" type="text" id="fecha_input-editar-profesional" name="fecha_input-editar-profesional" disabled>
+                        </div>
+
+                        <div class="col-md-6 p-0">
+                            <label for="hora_input-editar-profesional" class="col-12 text_label-formProf">Hora</label>
+                            <input class="form-control" type="time" id="hora_input-editar-profesional" name="hora_input-editar-profesional">
+                        </div>
+
+                    </form>
+                </div>
+
+                <!-- Sección botón Pagar -->
+                <div class="modal-footer section_btn_citas">
+                    <button type="submit" class="btnAgendar-popup" id="editar-cita-profesional">Guardar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <!-- Pop-up cancelar cita -->
-    <div class="modal fade modalE" id="cancelada-cita-profecional" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel3" aria-hidden="true">
+    <div class="modal fade modalE" id="cancelada-cita-modal-profecional" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel3" aria-hidden="true">
         <div class="modal-dialog modal-lg container_modal_cancelo" role="document">
             <div class="modal-content content_canceloCita">
                 <!-- Sección boton derecho de cierre "X" -->
