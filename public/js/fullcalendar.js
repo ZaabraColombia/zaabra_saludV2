de7e89ab67a764b4a14dfdd5f7d8d6436a5b6182
Nicolas-Moreno-arevalo <75846359+Nicolas-Moreno-arevalo@users.noreply.github.com>
2021-08-26 08:06:56 -0500

    arreglar recuadro azul en los eventos

--- public/js/fullcalendar.js
+++ public/js/fullcalendar.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
 
     var calendar = new FullCalendar.Calendar(calendarEl, {
+        
         selectable: true,
 
         //Parametro para la creación del botón agendar cita
@@ -23,9 +24,11 @@ document.addEventListener('DOMContentLoaded', function() {
         locale: 'es',
         events: [
             {
-              id: 'a',
-              title: 'my event',
-              start: '2021-08-22',
+                allDay: true,
+                id: 'a',
+                title: 'my event',
+                //start: '2021-08-27',
+                //startStr: '2021-08-27T07:00:00Z'
             }
         ],
         dateClick: function(info) {
@@ -49,5 +52,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
     if (calendarEl !=  null) {
         calendar.render();
+        setTimeout(function (){
+            console.log(calendar.getEvents());
+        }, 3000);
     }
 });
