731bf52152a455e69efa95c11fc0e9278c194a44
Nicolas-Moreno-arevalo <75846359+Nicolas-Moreno-arevalo@users.noreply.github.com>
2021-09-09 10:53:29 -0500

    mejorar buscador otra vez

--- public/js/filtroBusquedad.js
+++ public/js/filtroBusquedad.js
@@ -33,7 +33,7 @@ $(function(){
     }).data( "ui-autocomplete" )._renderItem = function( ul, item ) {
         return $( "<li></li>" )
             .data( "item.autocomplete", item )
-            .append( "<a href='"  + item.url + "'> <i class='" + item.icon + "'></i>" +item.label +"</a>" )
+            .append( "<a href='"  + item.url + "'><i class='" + item.icon + "'></i>" +item.label +"</a>" )
             .appendTo( ul );
 
     }
