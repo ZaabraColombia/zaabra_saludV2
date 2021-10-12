c87ed7ab33f0e0463f7ad0f3f9128c3b530331eb
jhonzaabra <72825636+jhonzaabra@users.noreply.github.com>
2021-10-12 14:10:22 -0500

    jhon f 12-10-2021

--- resources/views/instituciones/profesionales-institucion.blade.php
+++ resources/views/instituciones/profesionales-institucion.blade.php
@@ -47,9 +47,9 @@
         <div class="container_principal_instProf">
            <!-- Filter -->
            <ul id="filterControls" class="list-inline cbp-l-filters-alignRight text-center">
-               <li class="list-inline-item cbp-filter-item cbp-filter-item-active u-cubeportfolio__item asociado" data-filter="*">Asociados</li>
+               <li class="list-inline-item cbp-filter-item cbp-filter-item-active u-cubeportfolio__item asociado all_asociados" data-filter="*">Asociados</li>
                @foreach($especialidades as $item)
-                   <li class="list-inline-item cbp-filter-item u-cubeportfolio__item asociado" data-filter=".{{ Str::slug($item) }}">{{ $item }}</li>
+                   <li class="list-inline-item cbp-filter-item u-cubeportfolio__item asociado one_especiality" data-filter=".{{ Str::slug($item) }}">{{ $item }}</li>
                @endforeach
            </ul>
            <!-- End Filter -->
@@ -60,11 +60,11 @@
                     <div class="card tarjeta_instProf cbp-item {{ Str::slug($profesional->nombre_especialidad) }}">
                         <img class="img_perfil_instProf" src="{{ asset($profesional->foto_perfil_institucion) }}">
                         <div class="card-body content_tarjeta_instProf">
-                            <h2>{{$profesional->nombre_especialidad}}</h2>
-                            <h2>{{$profesional->nombre_especialidad}}</h2>
+                            <h2 class="show_especiality">{{$profesional->nombre_especialidad}}</h2>
+                            <h2 class="hidden_especiality">{{$profesional->nombre_especialidad}}</h2>
                             <h5 class="niega_uppercase">{{$profesional->primer_nombre}} {{$profesional->primer_apellido}}</h5>
-                            <p>Especialista en {{$profesional->nombre_especialidad}}</p>
-                            <p>Especialista en {{$profesional->nombre_especialidad}}</p>
+                            <p class="show_especiality">Especialista en {{$profesional->nombre_especialidad}}</p>
+                            <p class="hidden_especiality">Especialista en {{$profesional->nombre_especialidad}}</p>
                             <p>{{$profesional->nombre_universidad}}</p>
                             <h4>{{$profesional->cargo}}</h4>
                             <div class="content_btn_instprof">
@@ -88,6 +88,7 @@
         jQuery(document).ready( function() {
             jQuery('#grid-container').cubeportfolio({
                 filters: '#filterControls',
+                  
                 mediaQueries: [
                     {"width" : 1500, "cols" : 4},
                     {"width" : 1100, "cols" : 4},
@@ -96,6 +97,17 @@
                     {"width" : 300, "cols" : 1},
                 ]
                 
+            });
+        });
+
+        $(document).ready(function(){
+            $(".all_asociados").on( "click", function() {
+                $('.show_especiality').show(); 
+                $('.hidden_especiality').show(); 
+            });
+            $(".one_especiality").on( "click", function() {
+                $('.hidden_especiality').show(); 
+                $('.show_especiality').hide(); 
             });
         });
     </script>
