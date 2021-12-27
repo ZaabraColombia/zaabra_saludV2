f1fa46368cfcfeb34c81ff6ab571a545aea10ff6
Nicolas-Moreno-arevalo <75846359+Nicolas-Moreno-arevalo@users.noreply.github.com>
2021-12-27 08:40:20 -0500

    agrgar perfiles de institucion al home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,12 +7,12 @@
             @foreach ($objbannersprincipalHome as $objbannersprincipalHome)
                 <div class="swiper-slide ">
                     <img class="swiper-slide" src="{{URL::asset($objbannersprincipalHome->rutaImagenVenta)}}">
-                    
+
                     <div class="content_bannerPrincipal">
                         <h1 class="titulo_bannerPrincipal" style="color:{{($objbannersprincipalHome->color_titulo)}};">{{($objbannersprincipalHome->titulo_banner)}}</h1>
                         <p class="txt_bannerPrincipal" style="color:{{($objbannersprincipalHome->color_texto)}};">{{($objbannersprincipalHome->texto_banner)}}</p>
-                        <a class="boton_bannerPrincipal" type="submit" href="{{($objbannersprincipalHome->urlBoton_banner)}}" target="blank"  
-                            style="background-color:{{($objbannersprincipalHome->background_btn)}}; color:{{($objbannersprincipalHome->color_btn)}};"> 
+                        <a class="boton_bannerPrincipal" type="submit" href="{{($objbannersprincipalHome->urlBoton_banner)}}" target="blank"
+                            style="background-color:{{($objbannersprincipalHome->background_btn)}}; color:{{($objbannersprincipalHome->color_btn)}};">
                             {{($objbannersprincipalHome->texto_btn)}}
                         </a>
                     </div>
@@ -45,7 +45,7 @@
                 </a>
             </div>
         </div>
-        
+
         <div class="tarjeta_opcion animacion_tarjeta">
             <div class="cabecera_tarjeta">
                 <img class="icono_tarjeta" src="{{URL::asset('/img/home/instituciones-medicas.svg')}}">
@@ -54,7 +54,7 @@
             <div class="cuerpo_tarjeta">
                 <h5 class="subTitulo_contenido">Instituciones médicas</h5>
                 <p class="txt_contenido"> Clínicas, centros médicos y odontológicos, toma de exámenes y mucho más.</p>
-                
+
                 <a href="{{url('/Instituciones-Medicas')}}" class="btn_central_verde animación_boton">Ver más
                     <i class="fas fa-arrow-right pl-1"></i>
                 </a>
@@ -66,8 +66,8 @@
     @foreach ($objbannersparallaxHome as $objbannersparallaxHome)
         <div class="imagen_parallax" style="background-image: url( {{URL::asset($objbannersparallaxHome->rutaImagenVenta)}} );"></div>
     @endforeach
-   
-    <h1 class="titulo_principal">Nuestros Especialistas</h1>   
+
+    <h1 class="titulo_principal">Nuestros Especialistas</h1>
 
     <!--carousel especialistas-->
     <div class="swiper-container swiper_especialistas">
@@ -83,9 +83,16 @@
                     <p class="txt_especialista">{{$profesionales_home->primernombre}} {{$profesionales_home->primerapellido}}</p>
 
                     <div class="seccion_btn_central">
-                        <a href="{{url('PerfilProfesional/'.$profesionales_home->slug)}}" class="btn_grande_central_azul">Ver detalles
-                            <i class="fas fa-arrow-right pl-1"></i>
-                        </a>
+                        @if(!empty($profesionales_home->slug))
+                            <a href="{{route('PerfilProfesional', ['slug' => $profesionales_home->slug] )}}" class="btn_grande_central_azul">Ver detalles
+                                <i class="fas fa-arrow-right pl-1"></i>
+                            </a>
+                        @endif
+                        @if(!empty($profesionales_home->slug_institucion))
+                            <a href="{{route('PerfilInstitucion', ['slug' => $profesionales_home->slug_institucion])}}" class="btn_grande_central_azul">Ver detalles
+                                <i class="fas fa-arrow-right pl-1"></i>
+                            </a>
+                        @endif
                     </div>
                 </div>
             </div>
@@ -94,11 +101,11 @@
 
         <!-- If we need navigation buttons -->
         <div class="swiper-button-prev flecha_izquierda"></div>
-        <div class="swiper-button-next flecha_derecha"></div> 
+        <div class="swiper-button-next flecha_derecha"></div>
     </div>
 
     <!--banner triple-->
-    <div class="swiper-container swiper_triple">   
+    <div class="swiper-container swiper_triple">
         <div class="swiper-wrapper">
             @foreach ($objcarruselTriple as $objcarruselTriple)
                 <img class="swiper-slide"  src="{{URL::asset($objcarruselTriple->rutaImagenVenta)}}">
@@ -118,7 +125,7 @@
 
             <!-- If we need navigation buttons -->
             <div class="swiper-button-prev flecha_izquierda"></div>
-            <div class="swiper-button-next flecha_derecha"></div> 
+            <div class="swiper-button-next flecha_derecha"></div>
         </div>
     </section>
 @endsection
