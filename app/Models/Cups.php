87bd28f6d2db7f2f7ecd489bf0f68902ef850f49
Jonathan Wolfan <cesar.desarrollo@zaabra.com.co>
2022-02-23 15:55:09 -0500

    data cums-cups-cie10

---
+++ app/Models/Cups.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Hyn\Tenancy\Traits\UsesSystemConnection;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cups extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'code',
+        'description'
+    ];
+
+    protected $table = 'cups';
+
+    public $timestamps = false;
+
+}
