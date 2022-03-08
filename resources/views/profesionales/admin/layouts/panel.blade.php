c5afb8550c1e96559ea9650d6fb3ad70e0c0216e
jhonzaabra <72825636+jhonzaabra@users.noreply.github.com>
2022-03-08 09:14:46 -0500

    jhonf

--- resources/views/profesionales/admin/layouts/panel.blade.php
+++ resources/views/profesionales/admin/layouts/panel.blade.php
@@ -2,13 +2,13 @@
 
 @section('content')
     <div class="row m-0 p-0">
-        <div class="col-2 col-lg-3 d-none d-lg-block p-0 bg-light sidebar" style="height: fit-content;">
+        <div class="col-2 col-lg-3 d-none d-lg-block bg_white p-0">
             @include('profesionales.admin.layouts.menu')
         </div>
 
-        <div class="col-12 col-lg-9  panel-Administrativo_prof">
-            <div class="dropdown d-lg-none">
-                <a class="icon_menu_agenda dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></a>
+        <div class="col-12 col-lg-9 bg_gray p-0">
+            <div class="dropdown d-lg-none pt-3 pl-3">
+                <a class="dropdown-toggle menu_mobile" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></a>
 
                 <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
                     @include('profesionales.admin.layouts.menu')
