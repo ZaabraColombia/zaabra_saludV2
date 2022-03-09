1321d80872b2d233e43e9e897e8c40707f7ea1c2
jhonzaabra <72825636+jhonzaabra@users.noreply.github.com>
2022-03-09 16:02:53 -0500

    jhonf 09/03/2022 16:02
    Implementación de estilos y ajuste del responsive en todas las versiones de los dispositivos (celular, tablet y escritorio) en las tarjetas del panel principal de la agenda profesional.

--- resources/views/profesionales/admin/index.blade.php
+++ resources/views/profesionales/admin/index.blade.php
@@ -1,86 +1,93 @@
 @extends('profesionales.admin.layouts.panel')
 
 @section('contenido')
-    <div class="container py-4">
-        <div class="row m-0 justify-content-between">
-            <div class="col-6 p-2 border">
-                <a  href="">
-                    <div class="content__target">
-                        <img src="{{ asset('/img/agenda/panelPrincipal/mi-calendario.png') }}" alt="">
+    <div class="container py-4 px-lg-5">
+        <div class="row m-0 p-0">
+            <div class="col-12 col-lg-8 p-0 m-0">
+                <div class="row m-0 justify-content-between">
+                    <div class="col-6 col-md-4 col-lg-6 p-2 px-xl-4">
+                        <a  href="">
+                            <div class="content__target">
+                                <img src="{{ asset('/img/agenda/panelPrincipal/mi-calendario.png') }}" alt="">
 
-                        <h3 class="subtitle__lg">Mi Agenda</h3>
+                                <h3 class="subtitle__lg">Mi Agenda</h3>
+                            </div>
+                        </a>
                     </div>
-                </a>
+
+                    <div class="col-6 col-md-4 col-lg-6 p-2 px-xl-4">
+                        <a  href="">
+                            <div class="content__target">
+                                <img src="{{ asset('/img/agenda/panelPrincipal/mis-citas.jpg') }}" alt="">
+
+                                <h3 class="subtitle__lg">Mis Catálogos</h3>
+                            </div>
+                        </a>
+                    </div>
+
+                    <div class="col-6 col-md-4 col-lg-6 p-2 px-xl-4">
+                        <a  href="">
+                            <div class="content__target">
+                                <img src="{{ asset('/img/agenda/panelPrincipal/mis-pagos.png') }}" alt="">
+
+                                <h3 class="subtitle__lg">Mis Pagos</h3>
+                            </div>
+                        </a>
+                    </div>
+
+                    <div class="col-6 col-md-4 col-lg-6 p-2 px-xl-4">
+                        <a  href="">
+                            <div class="content__target">
+                                <img src="{{ asset('/img/agenda/panelPrincipal/mi-historia-clinica.png') }}" alt="">
+
+                                <h3 class="subtitle__lg">Mis Pacientes</h3>
+                            </div>
+                        </a>
+                    </div>
+
+                    <div class="col-6 col-md-4 col-lg-6 p-2 px-xl-4">
+                        <a  href="">
+                            <div class="content__target">
+                                <img src="{{ asset('/img/agenda/panelPrincipal/mis-examenes.jpg') }}" alt="">
+
+                                <h3 class="subtitle__lg">Mis Contactos</h3>
+                            </div>
+                        </a>
+                    </div>
+
+                    <div class="col-6 col-md-4 col-lg-6 p-2 px-xl-4">
+                        <a  href="">
+                            <div class="content__target">
+                                <img src="{{ asset('/img/agenda/panelPrincipal/mis-favoritos.png') }}" alt="">
+
+                                <h3 class="subtitle__lg">Mis Favoritos</h3>
+                            </div>
+                        </a>
+                    </div>
+                </div>
             </div>
 
-            <div class="col-6 p-2">
-                <a  href="">
-                    <div class="content__target">
-                        <img src="{{ asset('/img/agenda/panelPrincipal/mis-citas.jpg') }}" alt="">
-
-                        <h3 class="subtitle__lg">Mis Catálogos</h3>
+            <div class="col-12 col-lg-4 p-0 m-0">
+                <div class="row m-0">
+                    <div class="col-6 col-lg-12 py-3 px-2 py-lg-2">
+                        <a  href="">
+                            <div class="content__logos">
+                                <img src="{{ asset('/img/agenda/panelPrincipal/plm.png') }}" alt="">
+                            </div>
+                        </a>
                     </div>
-                </a>
-            </div>
 
-            <div class="col-6 p-2">
-                <a  href="">
-                    <div class="content__target">
-                        <img src="{{ asset('/img/agenda/panelPrincipal/mis-pagos.png') }}" alt="">
-
-                        <h3 class="subtitle__lg">Mis Pagos</h3>
+                    <div class="col-6 col-lg-12 py-3 px-2 py-lg-2">
+                        <a  href="">
+                            <div class="content__logos">
+                                <img src="{{ asset('/img/agenda/panelPrincipal/mipres-zaabra.png') }}" alt="">
+                            </div>
+                        </a>
                     </div>
-                </a>
-            </div>
-
-            <div class="col-6 p-2">
-                <a  href="">
-                    <div class="content__target">
-                        <img src="{{ asset('/img/agenda/panelPrincipal/mi-historia-clinica.png') }}" alt="">
-
-                        <h3 class="subtitle__lg">Mis Pacientes</h3>
-                    </div>
-                </a>
-            </div>
-
-            <div class="col-6 p-2">
-                <a  href="">
-                    <div class="content__target">
-                        <img src="{{ asset('/img/agenda/panelPrincipal/mis-examenes.jpg') }}" alt="">
-
-                        <h3 class="subtitle__lg">Mis Contactos</h3>
-                    </div>
-                </a>
-            </div>
-
-            <div class="col-6 p-2">
-                <a  href="">
-                    <div class="content__target">
-                        <img src="{{ asset('/img/agenda/panelPrincipal/mis-favoritos.png') }}" alt="">
-
-                        <h3 class="subtitle__lg">Mis Favoritos</h3>
-                    </div>
-                </a>
+                </div>
             </div>
         </div>
 
-        <div class="row m-0">
-            <div class="col-6 py-3 px-2">
-                <a  href="">
-                    <div class="d-flex justify-content-center">
-                        <img src="{{ asset('/img/agenda/panelPrincipal/plm.png') }}" alt="" width="105px">
-                    </div>
-                </a>
-            </div>
-
-            <div class="col-6 py-3 px-2">
-                <a  href="">
-                    <div class="d-flex justify-content-center">
-                        <img src="{{ asset('/img/agenda/panelPrincipal/mipres-zaabra.png') }}" alt="" width="105px">
-                    </div>
-                </a>
-            </div>
-        </div>
     </div>
 @endsection
 
