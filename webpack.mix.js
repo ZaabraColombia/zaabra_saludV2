04d23a20ed416ee4ec63db7711f2ec4da323433f
Jonathan Wolfan <cesar.desarrollo@zaabra.com.co>
2022-03-10 10:37:03 -0500

    arreglar producción de estilos y js

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,3 +14,7 @@ mix.options({ processCssUrls: false });
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .sourceMaps();
+
+if (mix.inProduction()) {
+    mix.version();
+}
