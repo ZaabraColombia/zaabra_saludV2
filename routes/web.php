b65f885e6d20a74999269c9bf5365613ddf3152c
jhonzaabra <72825636+jhonzaabra@users.noreply.github.com>
2022-04-05 11:07:27 -0500

    jhon f 05/04/2022 11:07
    Creación de la ruta en test para la vista principal de usuarios en la agenda instituciones.
    Creación de la vista principal de usuarios en la agenda instituciones.

--- routes/web.php
+++ routes/web.php
@@ -492,6 +492,11 @@ Route::get('/test-crear-servicio-institucion', function(){
     return view('instituciones.admin.configuracion.servicios.crear');
 });
 
+// Ruta de la vista usuarios 
+Route::get('/test-usuarios-institucion', function(){
+    return view('instituciones.admin.configuracion.usuarios.index');
+});
+
 // Ruta de la vista crear Usuario
 Route::get('/test-crear-usuario-institucion', function() {
     return view('instituciones.admin.configuracion.usuarios.crear');
