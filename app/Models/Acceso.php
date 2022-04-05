44501e8fffa2eef60cc8bce493b390cf45aebc94
Jonathan Wolfan <cesar.desarrollo@zaabra.com.co>
2022-04-05 15:22:09 -0500

    modificar modelos

--- app/Models/Acceso.php
+++ app/Models/Acceso.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Acceso extends Model
 {
@@ -23,5 +24,27 @@ class Acceso extends Model
     public function users(): \Illuminate\Database\Eloquent\Relations\BelongsToMany
     {
         return $this->belongsToMany(User::class, 'accesos_has_users', 'acceso_id', 'user_id');
+    }
+
+    /**
+     * Scope para delimitar los accesos
+     *
+     * @param Builder $query
+     * @return Builder
+     */
+    public function scopeInstitucion(Builder $query): Builder
+    {
+        return $query->where('tipo', 'like', 'institucion');
+    }
+
+    /**
+     * Scope para delimitar los accesos
+     *
+     * @param Builder $query
+     * @return Builder
+     */
+    public function scopeProfesional(Builder $query): Builder
+    {
+        return $query->where('tipo', 'like', 'profesional');
     }
 }
