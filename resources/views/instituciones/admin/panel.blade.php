aafc6d56493279984968f5db71ebba971bd4cb3e
Jonathan Wolfan <cesar.desarrollo@zaabra.com.co>
2022-04-07 10:18:30 -0500

    arreglar el menu y el panel

--- resources/views/instituciones/admin/panel.blade.php
+++ resources/views/instituciones/admin/panel.blade.php
@@ -5,87 +5,105 @@
         <div class="row m-0 p-0">
             <div class="col-12 col-lg-8 col-xl-9 p-0 m-0">
                 <div class="row m-0 justify-content-between">
-                    <!-- Profesionales -->
-                    <div class="col-6 col-md-4 col-lg-6 col-xl-4 p-2">
-                        <a  href='{{ route('institucion.profesionales.index') }}'>
-                            <div class="content__target_inst profesional_green">
-                                <span class="subtitle__lg">Profesionales</span>
-                            </div>
-                        </a>
-                    </div>
-                    <!-- Citas -->
-                    <div class="col-6 col-md-4 col-lg-6 col-xl-4 p-2">
-                        <a  href='{{-- route('institucion.agenda.citas') --}}'>
-                            <div class="content__target_inst cita_green">
-                                <span class="subtitle__lg">Citas</span>
-                            </div>
-                        </a>
-                    </div>
-                    <!-- Pagos -->
-                    <div class="col-6 col-md-4 col-lg-6 col-xl-4 p-2">
-                        <a  href='{{ route('institucion.pagos') }}'>
-                            <div class="content__target_inst pago_green">
-                                <span class="subtitle__lg">Pagos</span>
-                            </div>
-                        </a>
-                    </div>
-                    <!-- Pacientes -->
-                    <div class="col-6 col-md-4 col-lg-6 col-xl-4 p-2">
-                        <a  href='{{ route('institucion.pacientes') }}'>
-                            <div class="content__target_inst paciente_green">
-                                <span class="subtitle__lg">Pacientes</span>
-                            </div>
-                        </a>
-                    </div>
-                    <!-- Contactos -->
-                    <div class="col-6 col-md-4 col-lg-6 col-xl-4 p-2">
-                        <a  href='{{ route('institucion.contactos.index') }}'>
-                            <div class="content__target_inst contacto_green">
-                                <span class="subtitle__lg">Contactos</span>
-                            </div>
-                        </a>
-                    </div>
-                    <!-- Favoritos -->
-                    <div class="col-6 col-md-4 col-lg-6 col-xl-4 p-2">
-                        <a  href='{{ route('institucion.favoritos') }}'>
-                            <div class="content__target_inst favorito_green">
-                                <span class="subtitle__lg">Favoritos</span>
-                            </div>
-                        </a>
-                    </div>
+                    {{-- Profesionales --}}
+                    @can('accesos-institucion','ver-profesionales')
+                        <div class="col-6 col-md-4 col-lg-6 col-xl-4 p-2">
+                            <a  href='{{ route('institucion.profesionales.index') }}'>
+                                <div class="content__target_inst profesional_green">
+                                    <span class="subtitle__lg">Profesionales</span>
+                                </div>
+                            </a>
+                        </div>
+                    @endcan
+                    {{-- Citas --}}
+                    @can('accesos-institucion','ver-citas')
+                        <div class="col-6 col-md-4 col-lg-6 col-xl-4 p-2">
+                            <a  href='{{-- route('institucion.agenda.citas') --}}'>
+                                <div class="content__target_inst cita_green">
+                                    <span class="subtitle__lg">Citas</span>
+                                </div>
+                            </a>
+                        </div>
+                    @endcan
+                    {{-- Pagos --}}
+                    @can('accesos-institucion','ver-pagos')
+                        <div class="col-6 col-md-4 col-lg-6 col-xl-4 p-2">
+                            <a  href='{{ route('institucion.pagos') }}'>
+                                <div class="content__target_inst pago_green">
+                                    <span class="subtitle__lg">Pagos</span>
+                                </div>
+                            </a>
+                        </div>
+                    @endcan
+                    {{-- Pacientes --}}
+                    @can('accesos-institucion','ver-pacientes')
+                        <div class="col-6 col-md-4 col-lg-6 col-xl-4 p-2">
+                            <a  href='{{ route('institucion.pacientes') }}'>
+                                <div class="content__target_inst paciente_green">
+                                    <span class="subtitle__lg">Pacientes</span>
+                                </div>
+                            </a>
+                        </div>
+                    @endcan
+                    {{-- Contactos --}}
+                    @can('accesos-institucion','ver-contactos')
+                        <div class="col-6 col-md-4 col-lg-6 col-xl-4 p-2">
+                            <a  href='{{ route('institucion.contactos.index') }}'>
+                                <div class="content__target_inst contacto_green">
+                                    <span class="subtitle__lg">Contactos</span>
+                                </div>
+                            </a>
+                        </div>
+                    @endcan
+                    {{-- Favoritos --}}
+                    @can('accesos-institucion','favoritos')
+                        <div class="col-6 col-md-4 col-lg-6 col-xl-4 p-2">
+                            <a  href='{{ route('institucion.favoritos') }}'>
+                                <div class="content__target_inst favorito_green">
+                                    <span class="subtitle__lg">Favoritos</span>
+                                </div>
+                            </a>
+                        </div>
+                    @endcan
 
-                    <!-- Usuarios -->
-                    <div class="col-6 col-md-4 col-lg-6 col-xl-4 p-2">
-                        <a  href='#'>
-                            <div class="content__target_inst usuario_green">
-                                <span class="subtitle__lg">Usuarios</span>
-                            </div>
-                        </a>
-                    </div>
+                    {{-- Usuarios --}}
+                    @can('accesos-institucion','ver-usuarios')
+                        <div class="col-6 col-md-4 col-lg-6 col-xl-4 p-2">
+                            <a  href='{{ route('institucion.configuracion.usuarios.index') }}'>
+                                <div class="content__target_inst usuario_green">
+                                    <span class="subtitle__lg">Usuarios</span>
+                                </div>
+                            </a>
+                        </div>
+                    @endcan
 
-                    <!-- Convenios -->
-                    <div class="col-6 col-md-4 col-lg-6 col-xl-4 p-2">
-                        <a  href='{{ route('institucion.configuracion.convenios.index') }}'>
-                            <div class="content__target_inst convenio_green">
-                                <span class="subtitle__lg">Convenios</span>
-                            </div>
-                        </a>
-                    </div>
+                    {{-- Convenios --}}
+                    @can('accesos-institucion','ver-convenios')
+                        <div class="col-6 col-md-4 col-lg-6 col-xl-4 p-2">
+                            <a  href='{{ route('institucion.configuracion.convenios.index') }}'>
+                                <div class="content__target_inst convenio_green">
+                                    <span class="subtitle__lg">Convenios</span>
+                                </div>
+                            </a>
+                        </div>
+                    @endcan
 
-                    <!-- Servicios -->
-                    <div class="col-6 col-md-4 col-lg-6 col-xl-4 p-2">
-                        <a  href='{{ route('institucion.configuracion.servicios.index') }}'>
-                            <div class="content__target_inst servicio_green">
-                                <span class="subtitle__lg">Servicios</span>
-                            </div>
-                        </a>
-                    </div>
+                    {{-- Servicios --}}
+                    @can('accesos-institucion','ver-servicios')
+                        <div class="col-6 col-md-4 col-lg-6 col-xl-4 p-2">
+                            <a  href='{{ route('institucion.configuracion.servicios.index') }}'>
+                                <div class="content__target_inst servicio_green">
+                                    <span class="subtitle__lg">Servicios</span>
+                                </div>
+                            </a>
+                        </div>
+                    @endcan
                 </div>
             </div>
 
             <div class="col-12 col-lg-4 col-xl-3 p-0 m-0">
                 <div class="row m-0">
-                    <!-- PLM -->
+                    {{-- PLM --}}
                     <div class="col-6 col-lg-12 py-3 px-2 py-lg-2">
                         <a  href="https://www.prescripciontotal.com.co/consultorio-generico/login" target="_blank">
                             <div class="content__logos_inst">
@@ -93,7 +111,7 @@
                             </div>
                         </a>
                     </div>
-                    <!-- Mipres -->
+                    {{-- Mipres --}}
                     <div class="col-6 col-lg-12 py-3 px-2 py-lg-2">
                         <a  href="https://mipres.sispro.gov.co/MIPRESNOPBS/Login.aspx?ReturnUrl=%2fMIPRESNOPBS" target="_blank">
                             <div class="content__logos_inst">
