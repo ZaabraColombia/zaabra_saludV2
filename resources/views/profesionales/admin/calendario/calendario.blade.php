568bc2435fd385ed5c76cfe4ac38e56d9057bf7f
Jonathan Wolfan <cesar.desarrollo@zaabra.com.co>
2022-05-04 11:16:48 -0500

    reorganizar script en un js

--- resources/views/profesionales/admin/calendario/calendario.blade.php
+++ resources/views/profesionales/admin/calendario/calendario.blade.php
@@ -57,7 +57,9 @@
             <div class="col-12 col-lg-11 col-xl-8 p-0" id="alerta-general"></div>
 
             <div class="col-12 col-lg-11 col-xl-8 p-0 mb-3">
-                <div id="calendar"></div>
+                <div id="calendar" data-events="{{ route('profesional.agenda.calendario.ver-citas') }}"
+                     data-weekBissness='{!! json_encode($horario->horario) !!}'
+                     data-days='{!! json_encode($dias_disponibles) !!}'></div>
             </div>
         </div>
 
@@ -748,157 +750,12 @@
     <script src="{{ asset('fullCalendar/locales/es.js') }}"></script>
 
     <!-- Select 2 -->
+    <script src="{{ asset('js/calendario-profesional.js') }}"></script>
     <script src="{{ asset('js/filtro-ubicacion.js') }}"></script>
 
     <script>
-        moment.locale('es');
+
         document.addEventListener('DOMContentLoaded', function() {
-
-            //Iniciar data
-            var weekBusiness = '{!! json_encode($dias_disponibles) !!}';
-
-            var calendarEl = document.getElementById('calendar');
-            var calendar = new FullCalendar.Calendar(calendarEl, {
-                initialView: 'dayGridMonth',
-                businessHours: {!! json_encode($horario->horario) !!},
-                events: '{{ route('profesional.agenda.calendario.ver-citas') }}',
-                // Botones de mes, semana y día.
-                headerToolbar: {
-                    left: 'prev',
-                    center: 'title,dayGridMonth,timeGridWeek,timeGridDay,today',
-                    right: 'next'
-                },
-
-                slotLabelInterval: {
-                    minutes: 30
-                },
-                slotLabelFormat: {
-                    hour: 'numeric',
-                    minute: '2-digit',
-                    hour12: true
-                },
-                eventShortHeight: 15,
-                slotDuration: '00:15',
-                snapDuration: '02:00',
-                customButtons: {
-                    bloquear: {
-                        text: 'Bloquear',
-                        click: function() {
-
-                        }
-                    },
-                    // actualizar: {
-                    //     text: 'Actualizar',
-                    //     click: function() {
-                    //         calendar.refetchEvents();
-                    //         var message = {
-                    //             title:  'Hecho',
-                    //             text:   'Citas actualizadas'
-                    //         };
-                    //         $('#alerta-general').html(alert(message, 'success'));
-                    //     },
-                    //     //class: "button_blue_form"
-                    // }
-                },
-                // Propiedad para cambio de lenguaje
-                locale: 'es',
-                allDaySlot: false,
-
-                // Evento de mensaje de alerta
-                dateClick: function (event) {
-                    var today = moment();
-
-                    var day = moment(event.date);
-
-                    if (weekBusiness.includes(event.date.getDay()))
-                    {
-
-                        if (today.startOf('day').diff(day.startOf('day'), 'days') <= 0)
-                        {
-                            if (event.view.type === "dayGridMonth") {
-                                console.log(event.dateStr);
-                                $('#btn-day-clicked').data('date', event.dateStr);
-                                $('#btn-day-see').data('date', event.dateStr);
-                                $('#btn-reservar-agenda').data('date', event.dateStr);
-                                $('#span-day-clicked').html(day.format('MMMM D/YYYY'));
-
-                                $('#modal_dia_calendario').modal();
-                            }
-                        } else {
-                            calendar.changeView('timeGridDay', event.dateStr);
-                        }
-
-                    } else {
-                        alert('Día no laboral');
-                    }
-                },
-                selectable: false,
-                editable: false,
-
-                //Abrir evento
-                eventClick: function(info) {
-
-                    // $('.event-click-data').data('id', info.event._def.publicId)
-                    // $('#event-clicked').modal();
-                    $.ajax({
-                        data: { id: info.event._def.publicId},
-                        dataType: 'json',
-                        url: '{{ route('profesional.agenda.calendario.ver-cita') }}',
-                        headers: {
-                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                        },
-                        method: 'POST',
-                        success: function (res) {
-                            var modal;
-
-                            if (res.item.estado === 'reservado')
-                            {
-                                modal = $('#modal_ver_reserva');
-
-                                modal.find('.fecha_inicio').html(moment(res.item.fecha_inicio).format('dddd, D MMMM/YYYY'));
-                                modal.find('.fecha_fin').html(moment(res.item.fecha_fin).format('dddd, D MMMM/YYYY'));
-                                modal.find('.comentario').html(res.item.comentario);
-
-                                $('#btn-reserva-cancelar').data('id', res.item.id);
-                                $('#btn-reserva-editar').data('id', res.item.id);
-
-                                modal.modal();
-                            } else {
-
-                                modal = $('#modal_ver_cita');
-
-                                modal.find('.fecha').html(moment(res.item.fecha_inicio).format('dddd, D MMMM/YYYY'));
-                                modal.find('.hora').html(moment(res.item.fecha_inicio).format('hh:mm A') + '-' + moment(res.item.fecha_fin).format('hh:mm A'));
-                                modal.find('.nombre_paciente').html(res.item.nombre_paciente);
-                                modal.find('.tipo_cita').html(res.item.tipo_cita);
-                                modal.find('.modalidad').html(res.item.modalidad);
-                                modal.find('.correo').html(res.item.correo);
-                                modal.find('.numero_id').html(res.item.numero_id);
-
-                                $('#btn-cita-cancelar').data('id', res.item.id);
-                                $('#btn-cita-reagendar').data('id', res.item.id);
-                                $('#btn-cita-editar').data('id', res.item.id);
-                                $('#btn-cita-completar').data('id', res.item.id);
-
-                                modal.modal();
-                            }
-                        },
-                        error: function (res, status) {
-                            var response = res.responseJSON;
-                            $('#alerta-general').html(alert(response.message, 'danger'));
-                        }
-                    });
-                },
-
-                select: function(info) {
-
-                },
-
-                dayCellDidMount: function (date) {
-
-                },
-            });
-            calendar.render();
 
             //Bloquera dia
             $('#bloquear-dia').click(function (event) {
