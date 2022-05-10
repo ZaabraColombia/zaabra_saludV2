bb8ba09f51d63cb5a31e8d0a634b0b114ec9df4a
Jonathan Wolfan <cesar.desarrollo@zaabra.com.co>
2022-05-10 16:28:06 -0500

    sql

--- database/sql/update-2022-05.sql
+++ database/sql/update-2022-05.sql
@@ -3,7 +3,7 @@ DROP COLUMN `descanso`,
 DROP COLUMN `duracion`,
 ADD COLUMN `color_cita_pagada` VARCHAR(10) NULL AFTER `horario`,
 ADD COLUMN `color_cita_presencial` VARCHAR(10) NULL AFTER `color_cita_pagada`,
-ADD COLUMN `color_cita_agendada` VARCHAR(10) NULL AFTER `color_cita_precencial`,
+ADD COLUMN `color_cita_agendada` VARCHAR(10) NULL AFTER `color_cita_presencial`,
 ADD COLUMN `color_cita_cancelada` VARCHAR(10) NULL AFTER `color_cita_agendada`,
 ADD COLUMN `color_bloqueado` VARCHAR(10) NULL AFTER `color_cita_cancelada`,
 ADD COLUMN `dias_agenda` int(11) NULL AFTER `color_bloqueado`;
