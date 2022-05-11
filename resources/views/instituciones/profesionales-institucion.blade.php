21b8d46a1f6c07a46d265dd00fe11ac1f2d1d41f
Jonathan Wolfan <cesar.desarrollo@zaabra.com.co>
2022-05-11 10:44:15 -0500

    agregar modal para indicar que se debe logear como paciente

--- resources/views/instituciones/profesionales-institucion.blade.php
+++ resources/views/instituciones/profesionales-institucion.blade.php
@@ -134,6 +134,34 @@
             </div>
         </div>
     @endif
+    @if(session()->has('warning-profesional'))
+        <!-- Modal info iniciar login como paciente -->
+        <div class="modal fade" id="modal_profesional" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content modal_container">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+
+                    <div class="modal-body">
+                        <div class="elemento_infortmativo mt-3">
+                            <i data-feather="alert-triangle" class="informativo"></i>
+                        </div>
+
+                        <div class="text-center mt-4 px-3 py-0">
+                            <p class="black_light fs_text">Para agendar cita medica, inicie sesión como paciente.</p>
+                        </div>
+                    </div>
+
+                    <div class="modal-footer content_btn_center">
+                        <button type="button" class="button_transparent" data-dismiss="modal">Cerrar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endif
 @endsection
 
 @section('scripts')
@@ -147,6 +175,7 @@
 
     <script type="text/javascript">
         @if (session()->has('error-agenda')) $('#modal_agenda_no_disponible').modal();@endif
+        @if(session()->has('warning-profesional')) $('#modal_profesional').modal(); @endif
         @php $rest = request()->prof;@endphp
 
         jQuery(document).ready( function() {
