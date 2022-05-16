12d7b8b43b9e3aeb321e24d96d4f18dea0385329
Jonathan Wolfan <cesar.desarrollo@zaabra.com.co>
2022-05-16 08:50:18 -0500

    sql

--- database/sql/update-2022-05.sql
+++ database/sql/update-2022-05.sql
@@ -18,9 +18,6 @@ ALTER TABLE `zaabrac1_zaabra_salud_test`.`citas`
             REFERENCES `zaabrac1_zaabra_salud_test`.`convenios` (`id`)
             ON DELETE RESTRICT
             ON UPDATE RESTRICT;
-
-#Subido test
-#Subido production
 #Subido jhon 10-05-2022 16:24
 
 ALTER TABLE `zaabrac1_zaabra_salud_test`.`profesionales_instituciones`
@@ -33,5 +30,6 @@ ALTER TABLE `zaabrac1_zaabra_salud_test`.`users`
 ALTER TABLE `zaabrac1_zaabra_salud_test`.`auxiliares`
     ADD COLUMN `cargo` VARCHAR(50) NULL AFTER `celular`;
 
-
 #Subido cesar
+#Subido test
+#Subido production
