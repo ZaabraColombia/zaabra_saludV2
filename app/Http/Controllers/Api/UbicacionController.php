d08b68ac3cd15837c2dbbfe4ee30c9d1366387c0
Jonathan Wolfan <cesar.desarrollo@zaabra.com.co>
2022-05-17 14:08:34 -0500

    avance de la nueva estrucutra de datos

--- app/Http/Controllers/Api/UbicacionController.php
+++ app/Http/Controllers/Api/UbicacionController.php
@@ -3,15 +3,26 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Pais;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class UbicacionController extends Controller
 {
 
+    protected $url_paises_all   = "https://battuta.medunes.net/api/country/all/?key=";
+    protected $url_paises_      = "https://battuta.medunes.net/api/country/search/?country=:pais&key=";
+    protected $url_regiones = "https://battuta.medunes.net/api/region/:pais/all/?key=";
+    protected $url_ciudades = "https://battuta.medunes.net/api/city/:pais/search/?region=:region&key=";
 
-    public function paises()
+    public function paises(Request $request)
     {
+        $paises = Pais::query()
+            ->where('name', 'like', "%$request->pais%")
+            ->orWhere('code', 'like', "%$request->pais%")
+            ->get();
 
+        return response(['paises' => $paises], Response::HTTP_OK);
     }
 
     public function regiones(Request $request)
