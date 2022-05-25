f3153bd19cd144af1aa8bb72c60f38211c99c7a5
Jonathan Wolfan <cesar.desarrollo@zaabra.com.co>
2022-05-25 14:54:05 -0500

    buscador rutas listo

--- app/Http/Controllers/entidades/Admin/PanelController.php
+++ app/Http/Controllers/entidades/Admin/PanelController.php
@@ -3,12 +3,35 @@
 namespace App\Http\Controllers\entidades\Admin;
 use App\Http\Controllers\Controller;
 
+use App\Models\Ruta;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Route;
 
 class PanelController extends Controller {
 
-    public function index(){
+    public function index()
+    {
         return view('instituciones.admin.panel');
+    }
+
+
+    public function search(Request $request)
+    {
+        $rutas = Ruta::query()
+            ->select('rutas.*')
+            ->orderBy('titulo')
+            ->search($request->search)
+            ->validar()
+            ->institucion()
+            ->get();
+
+        $data = $rutas->map(function ($item){
+            if ($item->name) $item->ruta = route($item->name);
+            return $item;
+        });
+
+        return response($data, Response::HTTP_OK);
     }
 
 }
---
+++ app/Models/Ruta.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+
+class Ruta extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'titulo',
+        'url',
+        'name',
+        'slug',
+        'tipo'
+    ];
+
+    public $timestamps = false;
+
+    protected $table = 'rutas';
+
+    /**
+     * Permite filtrar por institución
+     *
+     * @param Builder $query
+     * @return Builder
+     */
+    public function scopeInstitucion(Builder $query): Builder
+    {
+        return $query->where('tipo', 'institucion');
+    }
+
+    public function scopeValidar(Builder $query)
+    {
+        //Los dos auxiliares
+        $rol = Auth::user()->roles()->whereIn('idrol', [4, 5])->first();
+        if (!empty($rol)) {
+            return $query->join('accesos', 'accesos.slug', '=', 'rutas.slug')
+                ->join('accesos_has_users', 'accesos_has_users.acceso_id', '=', 'accesos.id')
+                ->where('user_id', Auth::user()->id);
+        }
+
+        return $query;
+    }
+
+    /**
+     * permite validar si llego un search
+     *
+     * @param Builder $query
+     * @param $search
+     * @return Builder
+     */
+    public function scopeSearch(Builder $query, $search)
+    {
+        if ($search) return $query->where('titulo', 'like', "%{$search}%")
+            ->orWhere('url', 'like', "%{$search}%");
+
+        return $query;
+    }
+}
